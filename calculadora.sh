eaa7c155d9281b2afccf3c92a8a3de47a2ef9226
ChavesLucas16 <lucas.achaves1996@gmail.com>
2025-03-31 19:11:58 -0300

    Projeto calculadora completo
    Calculadora.sh  - Script em Bash que executa operações matemáticas básica s
    1_Projeto_Lucas_chaves_py - Código em Python que complementa a calculadora
    - Comandos usados em Linux para a execução e Script de execução, leitura.

---
+++ calculadora.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+echo "Calculadora Simples"
+echo "Digite o primeiro número : "
+read num1
+echo "Digite o segundo número : "
+read num2
+echo "Escolha a operação (+, -, /, *) : "
+read op
+
+case $op in
+    +) resultado=$(echo "$num1 + $num2" | bc) ;;
+    -) resultado=$(echo "$num1 - $num2" | bc) ;;
+    \*) resultado=$(echo "$num1 * $num2" |bc) ;;
+    /) resultado=$(echo "scale=2; $num1 / $num2" | bc) ;;
+    *) echo "Operaçao invalida!" ; exit 1 ;;
+esac
+
+
+echo "Resultado: $resultado"
